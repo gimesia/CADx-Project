0908b7e47b2cacb1e86b0216ca9680a39144122d
PabloBetancur28 <pablo1326699@gmail.com>
2024-11-24 01:12:01 +0100

    Normalization was added

--- 2_DL/code/modules/data_loader.py
+++ 2_DL/code/modules/data_loader.py
@@ -5,17 +5,31 @@ from torch.utils.data import DataLoader
 
 def get_data_loaders(train_dir, val_dir, batch_size):
 
+    normalize = transforms.Normalize(
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+    )
+
     train_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize(
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+    )
 
     ])
 
     test_transform = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        #Normalize images with statistis from the pre-trained dataset
+        transforms.Normalize( #Normalization operates on tensors and not PIL images
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+    )
         
     ])
 
