875fc862ec5227bd534b05077590c25e73f7e662
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-02 22:38:40 +0100

    Resnet 50 pretrained

--- 2_DL/code/modules/models_pretrained.py
+++ 2_DL/code/modules/models_pretrained.py
@@ -5,14 +5,14 @@ import torchvision.models as models
 
 class SkinLesionClassifier(nn.Module):
 
-    def __init__(self, num_classes = 2):
+    def __init__(self, num_classes = 1):
         super(SkinLesionClassifier, self).__init__()# this class inherits from nn.Module
 
-        self.model = models.resnet18(pretrained=True)
+        self.model = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V2)
 
         #Replace final FC layer
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     
     def forward(self, x):
-        return self.model(x)
+        return self.model(x).squeeze() #Ensure output is [batch_size]
