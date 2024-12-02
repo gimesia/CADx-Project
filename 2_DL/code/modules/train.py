3bff3c80b3cdaf64dfebd2f7db28aa6dec1d90e9
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-03 00:50:59 +0100

    Scheduler added

--- 2_DL/code/modules/train.py
+++ 2_DL/code/modules/train.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 
 
 
-def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs, device):
+def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, num_epochs, device):
 
     """
         Args:
@@ -31,6 +31,8 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
+        correct_train = 0
+        total_train = 0
 
         for inputs, labels, _  in train_loader:
             inputs, labels = inputs.to(device), labels.to(device).float()
@@ -44,15 +46,27 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             #Backprop and optimize
             loss.backward()
             optimizer.step()
+
+            #Calculate Loss
             running_loss += loss.item() * inputs.size(0)
 
+            preds = (torch.sigmoid(outputs) > 0.5).float()
+            correct_train += (preds == labels).sum().item()
+            total_train += labels.size(0)
+
+        if scheduler:
+            scheduler.step()
 
         #Average loss from all the batches
-        epoch_loss = running_loss/len(train_loader.dataset) # avg loss bc of batches
+        epoch_loss = running_loss / len(train_loader.dataset) # avg loss bc of batches
         train_losses.append(epoch_loss)
-
+        train_accuracy = correct_train / total_train
+        
+        #Validation
         model.eval()
         val_running_loss = 0.0
+        correct_val = 0
+        total_val = 0
 
         with torch.no_grad():
             for inputs, labels, _ in val_loader:
@@ -61,14 +75,21 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
                 loss = criterion(outputs, labels)
                 val_running_loss += loss.item() * inputs.size(0)
 
+                preds = (torch.sigmoid(outputs) > 0.5).float()
+                correct_val += (preds == labels).sum().item()
+                total_val += labels.size(0)
+
         
         #Average validation loss for epoch
         epoch_val_loss = val_running_loss / len(val_loader.dataset)
         val_losses.append(epoch_val_loss)
+        val_accuracy = correct_val / total_val
 
         print(f"Epoch {epoch + 1} / {num_epochs}",
               f"Training Loss: {epoch_loss:.4f}",
-              f"Validation Loss: {epoch_val_loss:.4f}")
+              f"Training Accuracy: {train_accuracy:.4f}",
+              f"Validation Loss: {epoch_val_loss:.4f}",
+              f"Validation Accuracy: {val_accuracy:.4f}")
         
 
     return model, train_losses, val_losses
