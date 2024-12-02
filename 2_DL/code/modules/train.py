78a1972d4eaa2fe02b0dd33047a31941cfe6010e
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-02 22:39:07 +0100

    Check validation set each epoch

--- 2_DL/code/modules/train.py
+++ 2_DL/code/modules/train.py
@@ -12,7 +12,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
         Args:
             model: Neural network to train.
             train_loader: DataLoader for training dataset.
-            test_loader: DataLoader for testing dataset.
+            val_loader: DataLoader for validation dataset.
             criterion: Loss function.
             optimizer: Optimization algorithm
             device: Device to use ('cuda' or 'cpu').
@@ -22,19 +22,20 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             Trained model.
     """
         
-
-
     model.to(device)
     
+    #Store losses
+    train_losses = []
+    val_losses = []
 
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
 
         for inputs, labels, _  in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            #Zero the parameter gradients
+            inputs, labels = inputs.to(device), labels.to(device).float()
 
+            #Zero the parameter gradients
             optimizer.zero_grad()
             #Forward pass
             outputs = model(inputs)
@@ -46,9 +47,28 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             running_loss += loss.item() * inputs.size(0)
 
 
-            #Continue with validation here
+        #Average loss from all the batches
         epoch_loss = running_loss/len(train_loader.dataset) # avg loss bc of batches
+        train_losses.append(epoch_loss)
 
-        print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {epoch_loss:.4f}") # Average loss across all batches
+        model.eval()
+        val_running_loss = 0.0
 
-    return model
+        with torch.no_grad():
+            for inputs, labels, _ in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device).float()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_running_loss += loss.item() * inputs.size(0)
+
+        
+        #Average validation loss for epoch
+        epoch_val_loss = val_running_loss / len(val_loader.dataset)
+        val_losses.append(epoch_val_loss)
+
+        print(f"Epoch {epoch + 1} / {num_epochs}",
+              f"Training Loss: {epoch_loss:.4f}",
+              f"Validation Loss: {epoch_val_loss:.4f}")
+        
+
+    return model, train_losses, val_losses
