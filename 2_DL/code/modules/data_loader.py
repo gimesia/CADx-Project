815244841a602d6506d4073636941b8683543517
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-04 20:05:22 +0100

    dataloader for test set

--- 2_DL/code/modules/data_loader.py
+++ 2_DL/code/modules/data_loader.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import Subset
@@ -14,9 +15,10 @@ class ImageFolderWIthPaths(datasets.ImageFolder):
 
         return image, label, path
 
-
-def get_data_loaders(train_dir, val_dir, batch_size, subset_percentage = 1.0):
+def get_data_loaders(train_dir, val_dir, batch_size, subset_percentage = 1.0, seed = 42):
     
+    g = torch.Generator()
+    g.manual_seed(seed)
 
     train_transform = transforms.Compose([
 
@@ -36,7 +38,7 @@ def get_data_loaders(train_dir, val_dir, batch_size, subset_percentage = 1.0):
 
     ])
 
-    test_transform = transforms.Compose([
+    valid_transform = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
         #Normalize images with statistis from the pre-trained dataset
@@ -49,11 +51,35 @@ def get_data_loaders(train_dir, val_dir, batch_size, subset_percentage = 1.0):
 
     #Load data from folders
     train_dataset = ImageFolderWIthPaths(train_dir, transform = train_transform)
-    val_dataset = ImageFolderWIthPaths(val_dir, transform = test_transform)
+    val_dataset = ImageFolderWIthPaths(val_dir, transform = valid_transform)
 
     #Dataloaders for datasets
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle = True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle = True, generator=g)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle = False)
 
     return train_loader, val_loader
+
+def get_data_loader_test(test_dir, batch_size, seed = 42):
+    g = torch.Generator()
+    g.manual_seed(seed)
+    
+
+    test_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        #Normalize images with statistis from the pre-trained dataset
+        transforms.Normalize( #Normalization operates on tensors and not PIL images
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+    )
+        
+    ])
+
+    #Load data from folders
+    test_dataset = ImageFolderWIthPaths(test_dir, transform = test_transform)
+
+    #Dataloaders for datasets
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle = True, generator = g)
+
+    return test_loader
 
