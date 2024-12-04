3200a2d492cf4efca7efc633b50f9de259a2ab86
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-04 09:46:30 +0100

    Model with 2 Linear outputs with droput and ReLU

--- 2_DL/code/modules/models_pretrained.py
+++ 2_DL/code/modules/models_pretrained.py
@@ -11,8 +11,14 @@ class SkinLesionClassifier(nn.Module):
         self.model = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V2)
 
         #Replace final FC layer
-        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        #self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
+        self.model.fc = nn.Sequential(
+            nn.Linear(self.model.fc.in_features, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, num_classes)
+        )
     
     def forward(self, x):
         return self.model(x).squeeze() #Ensure output is [batch_size]
